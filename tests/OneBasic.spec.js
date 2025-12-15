c4056bd20800b497a6e162c63e618c9ddaebfab0
Shiv Kumar Singh <shivkumarsingh2501@gmail.com>
2025-12-16 01:09:26 +0530

    Initial commit

---
+++ tests/OneBasic.spec.js
@@ -0,0 +1,23 @@
+const {test} = require('@playwright/test');
+
+
+test('My first UI test', async () => {
+  // automation steps go here
+});
+
+
+/*
+
+Explanation:
+------------
+
+test() takes two arguments:
+
+1. Test case name (string)
+
+Test function (contains automation logic)
+
+Each test() block is treated as one independent test case
+
+
+*/
---
+++ tests/TwoBasic.spec.js
@@ -0,0 +1,17 @@
+const {test} = require('@playwright/test');
+
+
+test('First playwright test',async function() {
+    console.log("This is my first Playwright test");
+    //javascript is asynchronous in nature 
+    // it means it does not wait for any task to complete and move to next task
+    // step1
+    // step2
+    // step3
+    // therefoere we need to use "async" and "await" keywords
+    // async - to declare a function as asynchronous : done in the function declaration
+    // await - to wait for a particular task to complete
+    
+
+});
+
